42ee4c8ec3177712de21a6fa66a86bb932c67e35
Jack <hk20130120515@163.com>
2020-10-11 00:44:35 +0800

    xxxx

---
+++ study/day13/02_apkshield.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+import sys
+import getopt
+
+
+class ApkShield(object):
+    def __init__(self):
+        object.__init__(self)
+
+    def main(self, argv):
+        """
+        取得命令行参数: python .\01_apkshield.py -s xxx.apk -p yyy.apk
+        """
+        shield_path = ''
+        payload_path = ''
+        try:
+            print("args:{}".format(argv))
+            opts, args = getopt.getopt(argv, "hs:p:", ["shield_path=", "payload_path="])
+        except getopt.GetoptError:
+            print('-s <shield apk path>', '-p <payload apk path>')
+            sys.exit(2)
+        print("args:{}".format(args))
+        print("opts:{}".format(opts))
+        for opt, arg in opts:
+            if opt == '-h':
+                print
+                '-s <shield apk path>', '-p <payload apk path>'
+                sys.exit()
+            elif opt in ("-s", "--shield_path"):
+                shield_path = arg
+            elif opt in ("-p", "--payload_path"):
+                payload_path = arg
+        print("shield_path:{}".format(shield_path))
+        print("payload_path:{}".format(payload_path))
+
+
+if __name__ == "__main__":
+    apkshield = ApkShield()
+    apkshield.main(argv=sys.argv[1:])
+    # apkshield.main(argv=sys.argv)
