3d73d71e8cdc74f637f79792e36d623c801f25ec
jie.chen <jie.chen@mobiledrivetech.com>
2023-03-01 12:33:17 +0800

    xxxx

--- study/day05/07_zip函数.py
+++ study/day05/07_zip函数.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     # 1. 两个列表(list)组成字典(dict)
     keys = ["name", "age", "sex", "city", "salary"]
     values = ["小龙女", "18", "女", "桃花岛", "1800"]
+    print("type:%s" % type(zip(keys, values)))
     xiao_long_nv = dict(zip(keys, values))
     print("type(xiao_long_nv) = {}".format(type(xiao_long_nv)))
     print(xiao_long_nv)
